c82f22cd8127fd882417a9ab746562e3c8522c33
Avram Cosmin <info@sprindo.co.uk>
2017-08-07 09:00:44 +0100

    Added ResourceAware class.

---
+++ src/Class/ResourceAware.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use Mindlahus\DoctrineResourceLayer\AbstractInterface\ResourceAbstract;
+use Mindlahus\DoctrineResourceLayer\AbstractInterface\ResourceInterface;
+
+class ResourceAware extends ResourceAbstract implements ResourceInterface
+{
+
+}
